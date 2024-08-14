01456f15fa78329fbd71e1245b5e011674e23937
STRONG-EASY <maratarenzi@gmail.com>
2024-08-14 10:49:44 +0200

    Retoques finales

--- Ejercicio5.js
+++ Ejercicio5.js
@@ -3,7 +3,7 @@ function rollDice(numCaras) {
   return Math.floor(Math.random() * numCaras) + 1
 }
 
-console.log(rollDice(6)) // Simula una tirada de dado de 6 caras (número aleatorio entre 1 y 6)
-console.log(rollDice(20)) // Simula una tirada de dado de 20 caras (número aleatorio entre 1 y 20)
+console.log(rollDice(5)) // Simula una tirada de dado de 5 caras (número aleatorio entre 1 y 5)
+console.log(rollDice(8)) // Simula una tirada de dado de 8 caras (número aleatorio entre 1 y 8)
 console.log(rollDice(10)) // Simula una tirada de dado de 10 caras (número aleatorio entre 1 y 10)
-console.log(rollDice(4)) // Simula una tirada de dado de 4 caras (número aleatorio entre 1 y 4)
+console.log(rollDice(6)) // Simula una tirada de dado de 6 caras (número aleatorio entre 1 y 6)
